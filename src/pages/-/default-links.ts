9256093af2c107626d8b371e3738a9c26caea2da
4MBL <74970679+4mbl@users.noreply.github.com>
2026-06-03 16:01:30 +0300

    feat: serve default data as static json (#188)

--- src/pages/-/default-links.ts
+++
@@ -1,9 +0,0 @@
-import defaultLinks from '../../data/default_links.json';
-
-export async function GET(): Promise<Response> {
-  return new Response(JSON.stringify(defaultLinks), {
-    headers: {
-      'content-type': 'application/json',
-    },
-  });
-}
